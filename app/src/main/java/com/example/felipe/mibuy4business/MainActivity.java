3162e437224954a806f474a401ba681435118bd6
felipe <felipeantunes542@gmail.com>
2018-05-07 23:48:55 -0300

    First commit

---
+++ app/src/main/java/com/example/felipe/mibuy4business/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.felipe.mibuy4business;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.setWebViewClient(new WebViewClient());
+
+        mWebView.loadUrl("https://projetocartolada.firebaseapp.com/");
+
+    }
+}
